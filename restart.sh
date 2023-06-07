286494524666ae84d98e9c7882e2d75a6302e498
Jorich <35774231+Yrkki@users.noreply.github.com>
2023-06-07 15:42:09 +0300

    feat(script): add restart

---
+++ restart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $'\033[1;33m'Running script restart
+echo ------------------------------------------------------$'\033[1;33m'
+echo
+
+. ./switch.sh
+. ./switch.sh
+
+echo
+echo $'\033[1;32m'Restart finished.$'\033[0m'
+
+echo
+# read  -n 1 -p "x" input
+# exit
